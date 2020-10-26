4279fde28678ef828867816cd5fe3dc08cd1668c
L.Walker <logantwalker95@gmail.com>
2020-10-26 17:18:41 -0400

    initial commit

---
+++ career-recruiting-specialists/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Navbar from './Components/Navbar/navbar';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar></Navbar>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ career-recruiting-specialists/src/Components/Navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = (props) =>{
+    return(
+        <div>
+            <h1>testing the navbar component</h1>
+        </div>
+    )
+}
+
+export default Navbar
