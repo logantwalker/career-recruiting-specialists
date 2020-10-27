87a4c566189d48aeea5bb550f24e10d6c7a15510
L.Walker <logantwalker95@gmail.com>
2020-10-27 19:27:19 -0400

    navbar and navitem component skeletons -- more functions to be added after routing

--- career-recruiting-specialists/src/Components/Navbar/navbar.js
+++ career-recruiting-specialists/src/Components/Navbar/navbar.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import './navbar.css';
+import 'materialize-css';
 
-const Navbar = (props) =>{
-    return(
-        <div>
-            <h1>testing the navbar component</h1>
-        </div>
+import BrandLogo from './assets/logo-image.jpg'
+import { Divider, Col, Row } from 'react-materialize';
+import NavItem from './NavItem/navitem'
+
+const Navbar = (props) => {
+    return (
+        <React.Fragment>
+            <div className='custom-navbar'>
+                <div className='nav-content'>
+                    <Row>
+                        <Col>
+                            <img height='75px' src={BrandLogo} alt='Career Recruiting Specialists Logo' />
+                        </Col>
+                        <Col>
+                            <NavItem>Home</NavItem>
+                        </Col>
+                        <Col>
+                            <NavItem
+                                id='1'
+                                title='For Employers'
+                                dropdown={true}>
+                                    <a className='dropdown-link' href='!#'>Placeholder</a>
+                                    <a className='dropdown-link' href='!#'>Placeholder</a>
+                                    <a className='dropdown-link' href='!#'>Placeholder</a>
+                            </NavItem>
+                        </Col>
+                        <Col>
+                            <NavItem
+                                id='2'
+                                title='For Job Seekers'
+                                dropdown={true}>
+                                    <a className='dropdown-link' href='!#'>Job Seekers FAQ</a>
+                                    <a className='dropdown-link' href='!#'>Current Job Openings</a>
+                                    <Divider />
+                                    <a className='dropdown-link' href='!#'>Online Application</a>
+                            </NavItem>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
 
