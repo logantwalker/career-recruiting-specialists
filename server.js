88ab675d047b935321d8e4ec3b0a64aa12db36e7
L.Walker <logantwalker95@gmail.com>
2020-12-03 14:42:48 -0500

    more changes to fix heroku deploy errors

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const routes = require("./routes");
-const session = require("express-session");
+// const session = require("express-session");
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+    
+// }
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('./client/build'));
 
 app.use(routes)
 
