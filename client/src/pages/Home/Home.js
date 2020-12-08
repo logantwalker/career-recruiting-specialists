7417ac3bf8a9adb7d6f8eddf7639b2f6d3487fd7
L.Walker <logantwalker95@gmail.com>
2020-12-08 16:29:33 -0500

    footer added. home page mock finished

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -107,9 +107,9 @@ const Home = () => {
                             </h2>
                             <p>
                                 "Not only did Joyce make sure to recruit candidates that met the technical requirements for the job, but she ensured that the candidates would be an excellent fit for our manufacturing culture.
-                                  She ensured the cultural fit by spending time upfront with me and the hiring manager and asking multiple detailed questions about the position responsibilities, goals, challenges and requirements.
-                                   I always feel confident at the end of our meetings that she knows exactly what we are looking for in our next hire..  
-                                   Simply the BEST.  Our company would not use anyone else."
+                                She ensured the cultural fit by spending time upfront with me and the hiring manager and asking multiple detailed questions about the position responsibilities, goals, challenges and requirements.
+                                I always feel confident at the end of our meetings that she knows exactly what we are looking for in our next hire..
+                                Simply the BEST.  Our company would not use anyone else."
                             </p>
                         </div>
                     </div>
@@ -120,8 +120,8 @@ const Home = () => {
                             </h2>
                             <p>
                                 “Joyce is an excellent recruiter who made the effort to get to know me as a person and consequently found me a job that fit well with my requirements.
-                                 She was quick to follow up and is an excellent communicator.
-                                 I would recommend her to anyone looking for a new position.” 
+                                She was quick to follow up and is an excellent communicator.
+                                I would recommend her to anyone looking for a new position.”
                             </p>
                         </div>
                     </div>
@@ -131,9 +131,9 @@ const Home = () => {
                                 Matt, Chemical Engineer
                             </h2>
                             <p>
-                                 “It is a real pleasure to work with Joyce. She provided me with support through all stages of job hunting.
-                                 I just have to come and perform on the interview and she took care of everything else (communicating desirable salary, chasing HR, etc.). 
-                                 Thank you, Joyce!”
+                                “It is a real pleasure to work with Joyce. She provided me with support through all stages of job hunting.
+                                I just have to come and perform on the interview and she took care of everything else (communicating desirable salary, chasing HR, etc.).
+                                Thank you, Joyce!”
                             </p>
                         </div>
                     </div>
@@ -143,24 +143,35 @@ const Home = () => {
                 <div className="container footer-content">
                     <div className="row">
                         <div className="col l5 s12">
-                            <h5 className="white-text">Contact Me</h5>
-                            <p className="grey-text text-lighten-4">If you want to work together or just learn more about me, check out these links!.</p>
+                            <h4 className="white-text">Contact Us!</h4>
+                            <h6 >
+                                <Icon tiny left={true}>access_time</Icon> 8:00 AM - 5:00 PM Monday - Friday
+                            </h6>
+                            <h6>
+                                <Icon tiny left={true}>phone</Icon>  706-840-0980
+                            </h6>
+                            <h6>
+                                <Icon tiny left={true}>mail</Icon> joyce@careerrecruitingspecialists.com
+                            </h6>
                         </div>
                         <div className="col l4 offset-l3 s12">
-                            <h5 className="white-text">Social Media:</h5>
-                            <ul>
-                                <li><a className="grey-text text-lighten-3" href="https://github.com/logantwalker" target="_blank"><i
-                                    className='material-icons tiny'>computer</i> GitHub</a></li>
-                                <li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/logantwalker37/"
-                                    target="_blank"><i className='material-icons tiny'>account_box</i> LinkedIn</a></li>
-                                <li><a className="grey-text text-lighten-3" href="#!"><i className='material-icons tiny'>folder</i> Resume</a></li>
-                            </ul>
+                            <h4 className="white-text">Social Media:</h4>
+                            <h6>
+                                <a className="grey-text text-lighten-3" href="https://github.com/logantwalker" >
+                                    <Icon tiny left={true}>class</Icon>FaceBook
+                                </a>
+                            </h6>
+                            <h6>
+                                <a className="grey-text text-lighten-3" href="https://github.com/logantwalker" >
+                                    <Icon tiny left={true}>business</Icon>LinkedIn
+                                </a>
+                            </h6>
                         </div>
                     </div>
                 </div>
                 <div className="footer-copyright">
                     <div className="container center-align">
-                        Career Recruiting Specialists © 2021
+                        <a className="grey-text text-lighten-3" href="https://github.com/logantwalker">Website by Walker Web Design © 2021</a>
                     </div>
                 </div>
             </footer>
